f301baf48af1db298671e301a829a613c551ea60
Jan Robert <jan.robert@cegeka.com>
2022-05-20 11:28:03 +0200

    feat: VSDSPUB-60 first version of synchronising LDES Client
    feat: VSDSPUB-60 first version of synchronising LDES Client

--- ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/client/services/LdesServiceImpl.java
+++ ldes-client/src/main/java/be/vlaanderen/informatievlaanderen/ldes/client/services/LdesServiceImpl.java
@@ -1,16 +1,23 @@
 package be.vlaanderen.informatievlaanderen.ldes.client.services;
 
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.riot.RDFParser;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.LinkedList;
 import java.util.List;
 
 import static be.vlaanderen.informatievlaanderen.ldes.client.valueobjects.LdesConstants.*;
+import static java.util.Arrays.stream;
 
 public class LdesServiceImpl implements LdesService {
     protected static final Resource ANY = null;
@@ -22,29 +29,35 @@ public class LdesServiceImpl implements LdesService {
     }
 
     @Override
-    public List<String[]> processNextPage() {
-        Model model = ModelFactory.createDefaultModel();
-
-        // Parsing the data
-        RDFParser.source(stateManager.getNextFragmentToProcess())
-                .forceLang(Lang.JSONLD11)
-                .parse(model);
-
-        // Sending members
-        List<String[]> ldesMembers = processLdesMembers(model);
-
-        // Queuing next pages
-        processRelations(model);
-
-        if (!stateManager.hasFragmentsToProcess()) {
-            stateManager.setFullyReplayed(true);
-        }
-
-        return ldesMembers;
+    public void populateFragmentQueue() {
+        stateManager.populateFragmentQueue();
     }
 
     @Override
-    public boolean hasPagesToProcess() {
+    public List<String[]> processNextFragment() {
+        try {
+            Model model = ModelFactory.createDefaultModel();
+
+            String fragmentToProcess = stateManager.getNextFragmentToProcess();
+
+            Long fragmentMaxAge = retrieveFragment(fragmentToProcess, model);
+
+            // Sending members
+            List<String[]> ldesMembers = processLdesMembers(model);
+
+            // Queuing next pages
+            processRelations(model);
+
+            stateManager.processFragment(fragmentToProcess, fragmentMaxAge);
+
+            return ldesMembers;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public boolean hasFragmentsToProcess() {
         return stateManager.hasFragmentsToProcess();
     }
 
@@ -76,9 +89,9 @@ public class LdesServiceImpl implements LdesService {
     protected void processRelations(Model model) {
         model.listStatements(ANY, W3ID_TREE_RELATION, ANY)
                 .forEach(relation -> stateManager.addNewFragmentToProcess(relation.getResource()
-                .getProperty(W3ID_TREE_NODE)
-                .getResource()
-                .toString()));
+                        .getProperty(W3ID_TREE_NODE)
+                        .getResource()
+                        .toString()));
     }
 
     private void populateRdfModel(Model model, Resource resource) {
@@ -88,6 +101,41 @@ public class LdesServiceImpl implements LdesService {
                 populateRdfModel(model, statement.getResource());
             }
         });
+    }
+
+    /**
+     * Retrieves fragment from URL and stores it into the Model whilst passing returning the max age (in seconds) of the fragment
+     *
+     * @param url URL to retrieve fragment from
+     * @param model Jena Model to populate based on the data from the provided url
+     * @return the max age (in seconds) of the fragment
+     */
+    private Long retrieveFragment(String url, Model model) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //Executing the Get request
+        HttpResponse httpresponse = httpclient.execute(new HttpGet(url));
+
+        RDFParser.source(httpresponse.getEntity().getContent())
+                .forceLang(Lang.JSONLD11)
+                .parse(model);
+
+        return stream(httpresponse.getHeaders("Cache-Control"))
+                .findFirst()
+                .map(header -> {
+                    if (stream(header.getElements())
+                            .anyMatch(headerElement -> "immutable".equals(header.getName()))) {
+                        return null;
+                    } else {
+                        return stream(header.getElements())
+                                .filter(headerElement -> "max-age".equals(headerElement.getName()))
+                                .findFirst()
+                                .map(HeaderElement::getValue)
+                                .map(Long::parseLong)
+                                .orElse(null);
+                    }
+                })
+                .orElse(null);
     }
 
 }
